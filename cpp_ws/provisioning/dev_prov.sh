55082a95d891c6ffa85fa8638774deae9bba9c63
gizi92 <simonzsolt0308@gmail.com>
2021-05-27 19:48:22 +0300

    adjustments, new workspace for pluralsight course

--- cpp_ws/provisioning/dev_prov.sh
+++ cpp_ws/provisioning/dev_prov.sh
@@ -12,12 +12,3 @@ apt-get upgrade -y
 apt-get install -y build-essential gdb cmake zip unzip git
 # install Criterion Framework
 apt-get install -y criterion-dev
-
-# Build GoogleTest with CMake
-# git clone https://github.com/google/googletest.git -b release-1.10.0
-# cd googletest               # Main directory of the cloned repository.
-# mkdir build                 # Create a directory to hold the build output.
-# cd build
-# cmake .. -DBUILD_GMOCK=OFF  # Generate native build scripts for GoogleTest, without GoogleMock
-# make
-# sudo make install           # Install in /usr/local/ by default
